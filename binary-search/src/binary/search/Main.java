4c8f3150732df9ee2dc10ac52a10b8b979c03dd6
Nhat Pham <nhat2up@gmail.com>
2023-01-06 08:42:23 +0700

    binary-search

---
+++ binary-search/src/binary/search/Main.java
@@ -0,0 +1,57 @@
+package binary.search;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Binary search = Search algorithm that finds the position
+        //                  of a target value within a sorted array
+        //                  Half of the array is eliminated during each "step"
+        
+        //          Advantages:
+        //          Good for large data sets
+        
+        //          Complexity: O(logn)
+        int array[] = new int[10000];
+        
+        int target = 42;
+        
+        for(int i = 0; i < array.length; i++){
+            array[i] = i;
+        }
+        
+//        int index = Arrays.binarySearch(array, target);
+        int index = binarySearch(array, target);
+        
+        if(index == -1){
+            System.out.println(target + " not found!");
+        } else{
+            System.out.println("Element found at: " + index);
+        }
+    }
+    
+    public static int binarySearch(int[] array, int target){
+        
+        int low = 0;
+        int high = array.length - 1;
+        
+        while(low <= high){
+            
+            int middle = low + (high - low) / 2;
+            int value = array[middle];
+            System.out.println("middle: " + value);
+            
+            if(value < target){
+                low = middle + 1;
+            }else if(value > target){
+                high = middle - 1;
+            } else{
+                return middle; //target found
+            }
+            
+        }
+        return -1; //target not found
+    }
+    
+}
