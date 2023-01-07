cb3bccd85dcc779ea71492602ec1ac2203f8cc4c
Nhat Pham <nhat2up@gmail.com>
2023-01-07 07:59:30 +0700

    insertion-sort

---
+++ insertion-sort/src/insertion/sort/Main.java
@@ -0,0 +1,45 @@
+package insertion.sort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Insertion sort = after comparing elements to the left
+        //                  shift  elements to the right to make room
+        //                  to insert a  value
+        
+        //                  Complexity: quaritic time O(n^2)
+        
+        //                  Small data set: decent
+        
+        //                  Large data set: BAD
+        
+        //              Less steps than Bubble sort
+        
+        //      Best case is O(n) compared to Selection Sort O(n^2)
+        
+        int[] array = {9, 1, 8, 2, 7, 3, 6, 5, 4};
+        
+        insertionSort(array);
+        
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+    
+    private static void insertionSort(int[] array){
+        
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            
+            while(j >= 0 && array[j] > temp){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+        
+    }
+    
+}
